7ba80699dce93ab591c60d6974e1bddd285e4b9a
LES0915 <silver_lives@naver.com>
2024-06-15 02:47:07 +0900

    basicpitch logic

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from utils.torch_utils import smart_inference_mode
 from convert import convert
 import asyncio
 from render import render_vextab_to_image
+from basicPitch import custom_predict_and_save
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
@@ -15,11 +16,35 @@ if str(ROOT) not in sys.path:
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 IMAGE_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.bmp', '.gif')
+AUDIO_EXTENSIONS = ('.mp3', '.wav', '.flac', '.ogg') 
 
 def handle_other_file(file_path):
-    # 이 함수는 이미지 파일이 아닌 경우에 호출됩니다.
     print(f"Handling non-image file: {file_path}")
-    # 필요한 작업을 여기에 추가하십시오.
+
+    midi_info = custom_predict_and_save(save_midi)
+    
+    # if file_path.lower().endswith(AUDIO_EXTENSIONS):
+    #     # Directory to save the results
+    #     output_directory = os.getcwd()
+
+    #     # Call predict_and_save function from basic_pitch
+    #     predict_and_save(
+    #         [file_path],
+    #         output_directory,
+    #         save_midi=True,
+    #         sonify_midi=False,
+    #         save_model_outputs=False,
+    #         save_notes=False
+    #     )
+        
+    #     # Path of the saved MIDI file
+    #     midi_file_path = os.path.join(output_directory, os.path.splitext(os.path.basename(file_path))[0] + '.midi')
+    #     print(f"MIDI file saved to: {midi_file_path}")
+        
+    #     # Additional processing of the MIDI file can be done here if needed
+    # else:
+    #     print(f"File format not supported: {file_path}")
+
 
 @smart_inference_mode()
 def run(
